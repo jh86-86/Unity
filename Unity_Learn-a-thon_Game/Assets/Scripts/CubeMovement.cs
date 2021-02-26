d430d61512230e25ad75656a044e7e81aa5f9a1d
JJ <jh_86@hotmail.co.uk>
2021-02-26 14:13:18 +0000

    jjlevel

---
+++ Unity_Learn-a-thon_Game/Assets/Scripts/CubeMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMovement : MonoBehaviour
+   
+{
+
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed* Time.deltaTime, 0, 0);
+    }
+}
